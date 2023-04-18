668082483500c9250c1b38a4e5afb60404f29bdb
Charles Conroy <conroycd@bc.edu>
2023-04-18 16:34:48 -0400

    Initial Commit

---
+++ Species/SpeciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeciesApp.swift
+//  Species
+//
+//  Created by Charley Conroy on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SpeciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
